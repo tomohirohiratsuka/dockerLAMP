9ae42fa59b8b9ddb7157284b837750d7f39cd61f
tomohirohiratsuka <tomohiro.hiratsuka@plan-b.co.jp>
2018-06-06 12:09:02 +0900

    スクレイピングベース＆ローカル開発環境

--- html/index.php
+++ html/index.php
@@ -1,1 +1,7 @@
-<?php phpinfo();
+<?php
+
+require_once("phpQuery-onefile.php");
+require_once("phpQuery-onefile.php");
+$html = file_get_contents("https://ja.wikipedia.org/wiki/%E3%82%A6%E3%82%A7%E3%83%96%E3%82%B9%E3%82%AF%E3%83%AC%E3%82%A4%E3%83%94%E3%83%B3%E3%82%B0");
+echo phpQuery::newDocument($html)->find("h1")->text();
+
